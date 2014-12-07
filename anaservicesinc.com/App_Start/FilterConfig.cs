08b4ba04d59d9f86b58dbe422b2f4906f79668d7
Ceranski, Michael <mikeceranski@gmail.com>
2014-12-07 15:54:44 -0500

    Modified to incorporate Mikes latest requests. Website now looks like business card

---
+++ anaservicesinc.com/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace anaservicesinc.com {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ anaservicesinc.com/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="anaservicesinc.com.MvcApplication" Language="C#" %>
